06bd45be79f889e582e27ab13cf6b70e54adcf07
Safya90 <safia5000@hotmail.com>
2025-05-08 10:49:42 +0400

    Add project files.

---
+++ OmanVehicleServiceBookingSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OmanVehicleServiceBookingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This is Oman Vehicle Service Booking System!");
+        }
+    }
+}
